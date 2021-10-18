11ee708e8fc86e288507eea18eac0b42bca5fcc9
Gopi3998 <91726200+Gopi3998@users.noreply.github.com>
2021-10-18 08:03:42 -0700

    Add files via upload

---
+++ Uppercase and Lowercase...py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Starting the program for count the upper and lower case'.center(80,'*'))
+sample = input('enter string: ')
+uppercase = 0
+lowercase = 0
+for ch in sample:
+    if str .isupper(ch):
+        uppercase+=1
+    elif str.islower(ch):
+        lowercase+=1
+print('No of uppercase character..',uppercase)
+print('No of lowercase character..',lowercase)
+print("THE END".center(80,'*'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
